d5ce0e96b76ffeece0728cfbbad77ee38549d911
zanyongxin <799612119@qq.com>
2020-12-02 07:37:10 +0800

    初始化项目

---
+++ config/config.js
@@ -0,0 +1,30 @@
+// export default {
+//     singular: true,
+//   }
+
+// export default {
+//     singular: true,
+//     routes: [{
+//       path: '/',
+//       component: './HelloWorld',
+//     }],
+//   }
+
+// export default {
+//   dva: {},
+//   antd: {},
+//   routes: [{
+//     path: '/',
+//     component: 'HelloWorld',
+//   }],
+//   singular: true,
+// };
+export default {
+  dva: {},
+  antd: {},
+  routes: [{
+    path: '/',
+    component: 'HelloWorld',
+  }],
+  singular: true,
+};
